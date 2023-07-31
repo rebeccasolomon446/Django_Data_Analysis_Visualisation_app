0ed181dbc729b5c09290c42dc9e9f78258e233f4
unknown <rebecca.solomon446@gmail.com>
2023-07-31 17:25:09 +0100

    adds POST method to form to prevent data security risks

--- myApp/views.py
+++ myApp/views.py
@@ -2,9 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def hello(request):
-    name = 'Rebecca'
-    marvel_characters = {'ironman', 'thor', 'black widow'}
-    context = {
-        'marvel_characters' : marvel_characters
-    }
-    return render(request,'myApp/index.html', context) #render method combines parameters, context can be accessed in template
+
+    return render(request,'myApp/index.html') #render method combines parameters, context can be accessed in template
