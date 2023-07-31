215e311fee089408fb9c92e3b4a0bca79cd661ad
unknown <rebecca.solomon446@gmail.com>
2023-07-31 15:46:06 +0100

    creates function in views.py file and connects urls.py to render resonse on web browser

--- mySite/settings.py
+++ mySite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'myapp',
+    'myApp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
