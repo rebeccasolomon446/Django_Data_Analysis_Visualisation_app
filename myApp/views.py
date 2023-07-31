b88420d0588938d0e01b3e0cb28c9d4e96460140
unknown <rebecca.solomon446@gmail.com>
2023-07-31 17:03:24 +0100

    includes dynamic rendering in html using context parameter and template syntax

--- myApp/views.py
+++ myApp/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def hello(request):
-    return render(request,'myApp/index.html')
+    name = 'Rebecca'
+    context = {
+        'name' : name
+    }
+    return render(request,'myApp/index.html', context) #render method combines parameters, context can be accessed in template
