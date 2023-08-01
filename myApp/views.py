075d91def56daba609a913ee7c89059935c13b76
unknown <rebecca.solomon446@gmail.com>
2023-08-01 17:26:17 +0100

    creates function to accept POST reuqests from user

--- myApp/views.py
+++ myApp/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def hello(request):
-
+    if (request.method == 'POST'):
+        name = request.POST.get('name')
+        print(name)
+    else:
+        print('GET request')
     return render(request,'myApp/index.html') #render method combines parameters, context can be accessed in template
