f163e873c276f89ba62c0cb840570328c751a9cf
unknown <rebecca.solomon446@gmail.com>
2023-08-02 19:11:46 +0100

    adds feature to delete repeated data objects

--- myApp/views.py
+++ myApp/views.py
@@ -6,20 +6,22 @@ from .models import Data
 # Create your views here.
 def hello(request):
     if (request.method == 'POST'):
-       file = request.FILES['file'] 
-       df = pd.read_csv(file)
-       json_data = df.reset_index().to_json(orient='records')
-       data = []
-       data = json.loads(json_data)
-       for d in data:
-        name = d['property_name']
-        price = d['property_price']
-        rent = d['property_rent']
-        emi = d['emi']
-        tax = d['tax']
-        exp = d['other_exp']
-        dt = Data(name=name, price=price, rent=rent, emi=emi,tax=tax, exp=exp)
-        dt.save()
+        prev_data = Data.objects.all()
+        prev_data.delete()
+        file = request.FILES['file'] 
+        df = pd.read_csv(file)
+        json_data = df.reset_index().to_json(orient='records')
+        data = []
+        data = json.loads(json_data)
+        for d in data:
+            name = d['property_name']
+            price = d['property_price']
+            rent = d['property_rent']
+            emi = d['emi']
+            tax = d['tax']
+            exp = d['other_exp']
+            dt = Data(name=name, price=price, rent=rent, emi=emi,tax=tax, exp=exp)
+            dt.save()
     else:
         print('GET request')
     return render(request,'myApp/index.html') #render method combines parameters, context can be accessed in template
