cc31317ec62b33e237462842c6ec117f07904e06
unknown <rebecca.solomon446@gmail.com>
2023-08-02 17:30:50 +0100

    reformatting data into indexed json to make it usable

--- myApp/views.py
+++ myApp/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import pandas as pd
+from pandas.io import json
 # Create your views here.
 def hello(request):
     if (request.method == 'POST'):
        file = request.FILES['file'] 
-       print(pd.read_csv(file))
+       df = pd.read_csv(file)
+       json_data = df.reset_index().to_json(orient='records')
+       data = []
+       data = json.loads(json_data)
+       print(data)
     else:
         print('GET request')
     return render(request,'myApp/index.html') #render method combines parameters, context can be accessed in template
