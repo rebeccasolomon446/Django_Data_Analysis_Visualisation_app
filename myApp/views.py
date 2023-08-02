354f5cd0ab7605e5ea08d07494a7350d850d9fae
unknown <rebecca.solomon446@gmail.com>
2023-08-02 19:05:32 +0100

    saves uploaded data from browser to database using ORM

--- myApp/views.py
+++ myApp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import pandas as pd
 from pandas.io import json
+from .models import Data
 # Create your views here.
 def hello(request):
     if (request.method == 'POST'):
@@ -10,7 +11,15 @@ def hello(request):
        json_data = df.reset_index().to_json(orient='records')
        data = []
        data = json.loads(json_data)
-       print(data)
+       for d in data:
+        name = d['property_name']
+        price = d['property_price']
+        rent = d['property_rent']
+        emi = d['emi']
+        tax = d['tax']
+        exp = d['other_exp']
+        dt = Data(name=name, price=price, rent=rent, emi=emi,tax=tax, exp=exp)
+        dt.save()
     else:
         print('GET request')
     return render(request,'myApp/index.html') #render method combines parameters, context can be accessed in template
