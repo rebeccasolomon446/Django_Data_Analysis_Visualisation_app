5e4269e9a2863ab35b7afbc936d1c91c4d6c20b6
unknown <rebecca.solomon446@gmail.com>
2023-08-02 16:15:06 +0100

    changed form input to accept files, added correct enctype in form tag to accept data input

--- myApp/views.py
+++ myApp/views.py
@@ -3,8 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 def hello(request):
     if (request.method == 'POST'):
-        name = request.POST.get('name')
-        print(name)
+        print(request.FILES['file']) #check for any files posted, incl input id
     else:
         print('GET request')
     return render(request,'myApp/index.html') #render method combines parameters, context can be accessed in template
